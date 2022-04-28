b4c3f186b62e22288a8566158bce4f89bfc78f9d
Angelica <angielopez4004@gmail.com>
2022-04-28 16:39:01 -0500

    Create Menu component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,22 +1,10 @@
-import logoIconik from "../../assets/images/logo.png";
 import Header from "../Header";
+import Menu from "../Menu";
 import './app.css';
 
 const App = () => (
   <main className="main">
-    <div className="menu">
-      <img className='menu__logo-iconik' src={logoIconik} alt="Iconik" />
-      <nav className="menu__container">
-        <span className="menu__link">Dashboard</span>
-        <span className="menu__link">Ingresos</span>
-        <span className="menu__link">Egresos</span>
-        <span className="menu__link">Trabajadores/as</span>
-        <span className="menu__link">Clientes</span>
-        <span className="menu__link">Citas</span>
-        <span className="menu__link">Productos</span>
-        <span className="menu__link">Configuracion</span>
-      </nav>
-    </div>
+    <Menu/>
     <Header />
     <section className="content">Este es el contenido.</section>
   </main>
