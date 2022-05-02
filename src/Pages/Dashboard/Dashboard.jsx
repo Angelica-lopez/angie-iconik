f7994b7a64598e2d24fd81d0baee27de8f8802bd
Angelica <angielopez4004@gmail.com>
2022-05-02 14:57:30 -0500

    Download icons to cards

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,4 +1,4 @@
-import TargetMenu from "../../components/Resume-card";
+import ResumeCard from "../../components/ResumeCard";
 import "./dashboard.css";
 
 const Dashboard = () => (
@@ -6,10 +6,14 @@ const Dashboard = () => (
     <h1 className="dashboard-title">Dashboard</h1>
     <h2 className="dashboard-text">Bienvenid@s a Iconik</h2>
     <div className="dashboard-cards">
-      <TargetMenu value="$1000000" cardName="Ingresos" iconName="money-in"></TargetMenu>
-      <TargetMenu value="$4000000" cardName="Egresos" iconName="money-in" backgroundCard="#EAC0C1"></TargetMenu>
-      <TargetMenu value="5" cardName="Clientes" iconName="money-in" backgroundCard="#C18585"></TargetMenu>
-      <TargetMenu value="10" cardName="Proximas citas" iconName="money-in" backgroundCard="#606060" marginCard="0"></TargetMenu>
+      <ResumeCard 
+        value="$1000000"
+        cardName="Ingresos"
+        iconName="money-in"
+      />
+      <ResumeCard value="$4000000" cardName="Egresos" iconName="money-out" backgroundCard="#EAC0C1"/>
+      <ResumeCard value="5" cardName="Clientes" iconName="group" backgroundCard="#C18585"/>
+      <ResumeCard value="10" cardName="Proximas citas" iconName="calendar-check" backgroundCard="#606060" marginCard="0"/>
     </div>
   </>
 );
