7172e5aff5185b9823350ae825f749ff13cd01bc
Angelica <angielopez4004@gmail.com>
2022-05-02 13:19:36 -0500

    Create content to component Dashboard

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,7 +1,17 @@
+import TargetMenu from "../../components/Resume-card";
 import "./dashboard.css";
 
 const Dashboard = () => (
-  <h1 className="dashboard">Dashboard</h1>
+  <>
+    <h1 className="dashboard-title">Dashboard</h1>
+    <h2 className="dashboard-text">Bienvenid@s a Iconik</h2>
+    <div className="dashboard-cards">
+      <TargetMenu value="$1000000" cardName="Ingresos" iconName="money-in"></TargetMenu>
+      <TargetMenu value="$4000000" cardName="Egresos" iconName="money-in" backgroundCard="#EAC0C1"></TargetMenu>
+      <TargetMenu value="5" cardName="Clientes" iconName="money-in" backgroundCard="#C18585"></TargetMenu>
+      <TargetMenu value="10" cardName="Proximas citas" iconName="money-in" backgroundCard="#606060" marginCard="0"></TargetMenu>
+    </div>
+  </>
 );
 
 export default Dashboard;
---
+++ src/components/Resume-card/ResumeCard.jsx
@@ -0,0 +1,12 @@
+import Icon from "../Icon";
+import "./resume-card.css";
+
+const ResumeCard = ({ value, cardName, iconName, backgroundCard, marginCard }) => (
+  <div className="resume-card" style={{backgroundColor: backgroundCard, margin: marginCard}}>
+    <Icon className="resume-card__icon" name={iconName} color="#fff" width={35} height={35}/>
+    <span className="resume-card__value">{value}</span>
+    <h2 className="resume-card__title">{cardName}</h2>
+  </div>
+);
+
+export default ResumeCard;
