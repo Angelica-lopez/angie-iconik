ca1eb9ad9af733e1b5b6fb69dca2c12061bc253b
Angelica <angielopez4004@gmail.com>
2022-05-02 15:06:27 -0500

    Close tags have no children

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -11,9 +11,25 @@ const Dashboard = () => (
         cardName="Ingresos"
         iconName="money-in"
       />
-      <ResumeCard value="$4000000" cardName="Egresos" iconName="money-out" backgroundCard="#EAC0C1"/>
-      <ResumeCard value="5" cardName="Clientes" iconName="group" backgroundCard="#C18585"/>
-      <ResumeCard value="10" cardName="Proximas citas" iconName="calendar-check" backgroundCard="#606060" marginCard="0"/>
+      <ResumeCard 
+        value="$4000000" 
+        cardName="Egresos" 
+        iconName="money-out" 
+        backgroundCard="#EAC0C1"
+      />
+      <ResumeCard 
+        value="5" 
+        cardName="Clientes" 
+        iconName="group" 
+        backgroundCard="#C18585"
+        />
+      <ResumeCard 
+        value="10" 
+        cardName="Proximas citas" 
+        iconName="calendar-check" 
+        backgroundCard="#606060" 
+        marginCard="0"
+      />
     </div>
   </>
 );
