c10af606489170ea9195670f84bd7b67319d3aff
Angelica <angielopez4004@gmail.com>
2022-05-04 16:17:55 -0500

    Order props to ResumeCard

--- src/components/ResumeCard/ResumeCard.jsx
+++ src/components/ResumeCard/ResumeCard.jsx
@@ -1,7 +1,15 @@
 import Icon from "../Icon";
 import "./resume-card.css";
 
-const ResumeCard = ({ value, title, iconName, backgroundCard, marginCard, widthCard, heightCard }) => (
+const ResumeCard = ({ 
+  value, 
+  title, 
+  iconName, 
+  backgroundCard, 
+  marginCard, 
+  widthCard, 
+  heightCard,
+}) => (
   <div className="resume-card" style={{backgroundColor: backgroundCard, margin: marginCard}}>
     <Icon className="resume-card__icon" name={iconName} color="#fff" width={widthCard} height={heightCard}/>
     <span className="resume-card__value">{value}</span>
